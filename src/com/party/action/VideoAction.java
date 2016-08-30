81e940c3c9a163f8c2606d6377b58f8e2728bc94
chenlwh <chenlwh@cn.ibm.com>
2016-08-30 17:35:40 +0800

    base line

--- src/com/party/action/VideoAction.java
+++ src/com/party/action/VideoAction.java
@@ -5,9 +5,16 @@
  */
 package com.party.action;
 
+import com.party.util.PartyUtil;
+
 /**
  * 
  */
 public class VideoAction extends BaseAction {
+	
+	public String execute(){
+		
+		return PartyUtil.SUCCESS;
+	}
 
 }
